24be419f49da62a75160dd1b9b5088fadda831c7
bahrijar <muhammadbahrijar4@gmail.com>
2020-07-04 13:56:40 +0700

    test

--- app/src/main/java/com/bahrijar/invetarisapp/activity/LoginActivity.java
+++ app/src/main/java/com/bahrijar/invetarisapp/activity/LoginActivity.java
@@ -3,6 +3,8 @@ package com.bahrijar.invetarisapp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.transition.Fade;
 import android.view.View;
 import android.view.animation.Animation;
@@ -18,39 +20,45 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.bahrijar.invetarisapp.R;
 import com.bahrijar.invetarisapp.model.User;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 public class LoginActivity extends AppCompatActivity {
 
     Animation bottom_to_top;
     ConstraintLayout form_login;
     Button vBtnLogin;
-    EditText vNip, vPassword;
+    private TextInputEditText vNip, vPassword;
+
+    private TextInputLayout txtLayoutNip, txtLayoutPassword;
 
     Handler handler;
     Runnable runnable;
-
-    String iUsernip, iUserpassword;
-    DatabaseReference mDatabase;
-
-    String USERNIP_KEY = "usernipkey";
-    String usernip_key = "";
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mDatabase = FirebaseDatabase.getInstance().getReference("User");
 
+        init();
+
+    }
+
+    private void init() {
         form_login = findViewById(R.id.form_login);
         vBtnLogin = findViewById(R.id.btn_login);
         vNip = findViewById(R.id.edt_nip);
         vPassword = findViewById(R.id.edt_password);
+        txtLayoutNip = findViewById(R.id.txt_layout_nip);
+        txtLayoutPassword = findViewById(R.id.txt_layout_password);
 
         // Load Animations
         bottom_to_top = AnimationUtils.loadAnimation(this, R.anim.bottom_to_top);
@@ -60,7 +68,6 @@ public class LoginActivity extends AppCompatActivity {
         fade.excludeTarget(android.R.id.background, true);
         getWindow().setEnterTransition(fade);
 
-
         // Run Animasi
         form_login.startAnimation(bottom_to_top);
 
@@ -68,63 +75,82 @@ public class LoginActivity extends AppCompatActivity {
         vBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finishAffinity();
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
 
+                if (validate()) {
 
-//                iUsernip = vNip.getText().toString();
-//                iUserpassword = vPassword.getText().toString();
-//
-//                if (iUsernip.equals("")) {
-//                    vNip.setError("Masukkan NIP Anda");
-//                    vNip.requestFocus();
-//                } else if (iUserpassword.equals("")) {
-//                    vPassword.setError("Masukkan Password Anda");
-//                    vPassword.requestFocus();
-//                } else {
-//                    pushLogin(iUsernip, iUserpassword);
-//                }
+                }
+//                finishAffinity();
+
+                vNip.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        if (!vNip.getText().toString().isEmpty()) {
+                            txtLayoutNip.setErrorEnabled(false);
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
+
+                vPassword.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        if (!vPassword.getText().toString().isEmpty()) {
+                            txtLayoutPassword.setErrorEnabled(false);
+                        }
+                        else if (vPassword.getText().toString().length() > 7) {
+                            txtLayoutPassword.setErrorEnabled(false);
+                        }
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
 
             }
         });
 
     }
 
-//    private void pushLogin(final String iUsernip, final String iUserpassword) {
-//        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                if (dataSnapshot.child(iUsernip).exists()) {
-//                    if (!iUsernip.isEmpty()) {
-//                        User user = dataSnapshot.child(iUsernip).getValue(User.class);
-//                        if (user.getUserpassword().equals(iUserpassword)) {
-//                            Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
-//                            finishAffinity();
-//
-//                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                            intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                            startActivity(intent);
-//                        } else {
-//                            Toast.makeText(LoginActivity.this, "Password Salah", Toast.LENGTH_LONG).show();
-//                        }
-//                    } else {
-//                        Toast.makeText(LoginActivity.this, "User tidak ada", Toast.LENGTH_LONG).show();
-//                    }
-//                }
-//                else {
-//                    Toast.makeText(LoginActivity.this, "User tidak ada", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-//
-//    }
+    private boolean validate() {
+        if (vNip.getText().toString().isEmpty()) {
+            txtLayoutNip.setErrorEnabled(true);
+            txtLayoutNip.setError("Nip tidak boleh kosong");
+            return false;
+        }
+       else if (vPassword.getText().toString().isEmpty()) {
+            txtLayoutPassword.setErrorEnabled(true);
+            txtLayoutPassword.setError("Password tidak boleh kosong");
+            return false;
+        }
+        else if (vPassword.getText().toString().length() < 8) {
+            txtLayoutPassword.setErrorEnabled(true);
+            txtLayoutPassword.setError("Password minimal 8 karakter");
+            return false;
+
+        } else  {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+        return true;
+    }
 
 
     @Override
